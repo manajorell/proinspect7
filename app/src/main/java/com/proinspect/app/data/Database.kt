b95d41ff6ac9baf7b60182d9418d85829b01434c
manajorell <37649610+manajorell@users.noreply.github.com>
2026-03-07 13:36:01 -0500

    Update Database.kt

--- app/src/main/java/com/proinspect/app/data/Database.kt
+++ app/src/main/java/com/proinspect/app/data/Database.kt
@@ -57,7 +57,7 @@ interface AppSettingsDao {
 
 @Database(
     entities = [Report::class, InspectionItem::class, InspectionPhoto::class, AppSettings::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class ProInspectDatabase : RoomDatabase() {
